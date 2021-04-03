376b39eb94722648f8de34187b666626cc373f33
IMPixel <asemenov.ca@gmail.com>
2021-04-03 10:32:47 -0400

    Testing and added a control panel.
    There are gonna be buttons like menu in the control panel.

--- src/createGameBar.js
+++ src/createGameBar.js
@@ -9,7 +9,7 @@
 //just an example:
 //[{image: "https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg", name: "Title", link: "link"}];
 
-let games = [{image: "https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg", name: "Title", link: "link"} ]
+let games = [{image: "https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg", name: "Title", link: "link"}]
 
 let body = document.getElementsByTagName("BODY")[0];
 
